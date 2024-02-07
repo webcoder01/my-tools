43659b0d340ba8661494da66c32270aa97428439
Antoine Raymond <antoine.raymond@teksial.com>
2024-02-07 14:24:21 +0100

    feat(account manager): add accounts and transactions to fixtures

--- src/DataFixtures/AccountManagerFixtures.php
+++ src/DataFixtures/AccountManagerFixtures.php
@@ -2,9 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\AccountManager\Account\Provider\Entity\Account;
 use App\AccountManager\Budget\Infrastructure\Entity\Budget;
 use App\AccountManager\Budget\Infrastructure\Entity\BudgetCategory;
 use App\AccountManager\Budget\Infrastructure\Entity\BudgetType;
+use App\AccountManager\Transaction\Provider\Entity\Transaction;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -19,11 +21,16 @@ class AccountManagerFixtures extends Fixture implements DependentFixtureInterfac
   private BudgetType $houseInsuranceType;
   private BudgetType $vacationType;
 
+  private Account $mainAccount;
+  private Account $savingsAccount;
+
   public function load(ObjectManager $manager): void
   {
     $this->loadBudgetCategories($manager);
     $this->loadBudgetTypes($manager);
     $this->loadBudgets($manager);
+    $this->loadAccounts($manager);
+    $this->loadTransactions($manager);
 
     $manager->flush();
   }
@@ -96,5 +103,60 @@ class AccountManagerFixtures extends Fixture implements DependentFixtureInterfac
     $vacationBudget->setYear($year);
     $vacationBudget->setAssignedAmount('200.00');
     $vacationBudget->setAvailableAmount('-25.00');
+  }
+
+  private function loadAccounts(ObjectManager $manager): void
+  {
+    $this->mainAccount = new Account();
+    $this->mainAccount->setName('Compte courant CCP');
+    $this->mainAccount->setBalance('2000.00');
+    $this->mainAccount->setUser($this->getReference(UserFixtures::DEFAULT_USER_REFERENCE));
+    $manager->persist($this->mainAccount);
+
+    $this->savingsAccount = new Account();
+    $this->savingsAccount->setName('Livret A');
+    $this->savingsAccount->setBalance('10000.00');
+    $this->savingsAccount->setUser($this->getReference(UserFixtures::DEFAULT_USER_REFERENCE));
+    $manager->persist($this->savingsAccount);
+  }
+
+  private function loadTransactions(ObjectManager $manager): void
+  {
+    $firstVacationTransaction = new Transaction();
+    $firstVacationTransaction->setPayee('Auchan');
+    $firstVacationTransaction->setAmount('-50.99');
+    $firstVacationTransaction->setChecked(true);
+    $firstVacationTransaction->setBudgetType($this->vacationType);
+    $firstVacationTransaction->setAccount($this->mainAccount);
+    $manager->persist($firstVacationTransaction);
+
+    $secondVacationTransaction = new Transaction();
+    $secondVacationTransaction->setPayee('Formule 1');
+    $secondVacationTransaction->setAmount('-120.00');
+    $secondVacationTransaction->setComment('Séjour à l\'hôtel');
+    $secondVacationTransaction->setBudgetType($this->vacationType);
+    $secondVacationTransaction->setAccount($this->mainAccount);
+    $manager->persist($secondVacationTransaction);
+
+    $thirdVacationTransaction = new Transaction();
+    $thirdVacationTransaction->setPayee('Dominos Pizza');
+    $thirdVacationTransaction->setAmount('-12.50');
+    $thirdVacationTransaction->setBudgetType($this->vacationType);
+    $thirdVacationTransaction->setAccount($this->mainAccount);
+    $manager->persist($thirdVacationTransaction);
+
+    $fourthVacationTransaction = new Transaction();
+    $fourthVacationTransaction->setPayee('Circuit tour');
+    $fourthVacationTransaction->setAmount('-101.51');
+    $fourthVacationTransaction->setComment('Stage de pilotage sur Porsche');
+    $fourthVacationTransaction->setBudgetType($this->vacationType);
+    $fourthVacationTransaction->setAccount($this->mainAccount);
+    $manager->persist($fourthVacationTransaction);
+
+    $savingsTransaction = new Transaction();
+    $savingsTransaction->setPayee('Banque');
+    $savingsTransaction->setAmount('200.00');
+    $savingsTransaction->setAccount($this->savingsAccount);
+    $manager->persist($savingsTransaction);
   }
 }
